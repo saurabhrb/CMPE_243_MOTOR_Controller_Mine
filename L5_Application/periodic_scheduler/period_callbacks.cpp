d65526a6d06df471f95dcc6ec5724451029cabae
Abhilash Tuse <abhilashtuse@gmail.com>
2017-10-24 23:33:25 -0700

    Revert "Master controller periodic callback"
    This reverts commit 8a7c83f49a5826f2593ecd73eb77bea79457fce4.

--- L5_Application/periodic_scheduler/period_callbacks.cpp
+++ L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -29,34 +29,9 @@
  */
 
 #include <stdint.h>
-#include <string.h>
 #include "io.hpp"
 #include "periodic_callback.h"
-#include "can.h"
-#include "_can_dbc/generated_can.h"
 
-bool restart_can = false;
-
-void busoff_callback(uint32_t register_value)
-{
-    (void) register_value;
-    restart_can = true;
-}
-
-void dataovr_callback(uint32_t register_value)
-{
-    (void) register_value;
-}
-
-bool dbc_app_send_can_msg(uint32_t mid, uint8_t dlc, uint8_t bytes[8])
-{
-    can_msg_t can_msg = { 0 };
-    can_msg.msg_id                = mid;
-    can_msg.frame_fields.data_len = dlc;
-    memcpy(can_msg.data.bytes, bytes, dlc);
-
-    return CAN_tx(can1, &can_msg, 0);
-}
 
 
 /// This is the stack size used for each of the period tasks (1Hz, 10Hz, 100Hz, and 1000Hz)
@@ -73,11 +48,6 @@ const uint32_t PERIOD_MONITOR_TASK_STACK_SIZE_BYTES = (512 * 3);
 /// Called once before the RTOS is started, this is a good place to initialize things once
 bool period_init(void)
 {
-    CAN_init(can1, 100, 25, 25, busoff_callback, dataovr_callback);
-
-    CAN_bypass_filter_accept_all_msgs();
-    CAN_reset_bus(can1);
-
     return true; // Must return true upon success
 }
 
@@ -94,90 +64,24 @@ bool period_reg_tlm(void)
  * The argument 'count' is the number of times each periodic task is called.
  */
 
-bool start_sent = false;
-
 void period_1Hz(uint32_t count)
 {
-    if(restart_can == true)
-    {
-        restart_can = false;
-        CAN_reset_bus(can1);
-        LE.toggle(1);
-    }
-    can_msg_t can_msg;
-    BRIDGE_START_STOP_t bridge_data = {0};
-    MASTER_CONTROL_t start_cmd;
-    start_cmd.MASTER_CONTROL_cmd = DRIVER_HEARTBEAT_cmd_START;
-
-    if(start_sent == false)
-    {
-        if(CAN_rx(can1, &can_msg, 0))
-        {
-            // Form the message header from the metadata of the arriving message
-            dbc_msg_hdr_t can_msg_hdr;
-            can_msg_hdr.dlc = can_msg.frame_fields.data_len;
-            can_msg_hdr.mid = can_msg.msg_id;
-
-            // Attempt to decode the message
-            dbc_decode_BRIDGE_START_STOP(&bridge_data, can_msg.data.bytes, &can_msg_hdr);
-
-            if(bridge_data.BRIDGE_START_STOP_cmd == 1)
-            {
-                LE.set(2, true);
-                dbc_encode_and_send_MASTER_CONTROL(&start_cmd);
-                start_sent = true;
-            }
-        }
-    }
+    LE.toggle(1);
 }
 
 void period_10Hz(uint32_t count)
 {
-    can_msg_t can_msg;
-    SENSOR_DATA_t sensor_data;
-    MOTOR_UPDATE_t motor_update;
-    motor_update.MOTOR_speed = 10;
-    motor_update.MOTOR_turn_angle = 0;
-
-    if(start_sent == true)
-    {
-        if(CAN_rx(can1, &can_msg, 0))
-        {
-            LE.toggle(3);
-
-            // Form the message header from the metadata of the arriving message
-            dbc_msg_hdr_t can_msg_hdr;
-            can_msg_hdr.dlc = can_msg.frame_fields.data_len;
-            can_msg_hdr.mid = can_msg.msg_id;
-
-            dbc_decode_SENSOR_DATA(&sensor_data, can_msg.data.bytes, &can_msg_hdr);
-
-            if(sensor_data.SENSOR_left_sensor > 1 && sensor_data.SENSOR_left_sensor < 10)
-            {
-                motor_update.MOTOR_turn_angle = 30;
-            }
-            else if(sensor_data.SENSOR_right_sensor > 1 && sensor_data.SENSOR_right_sensor < 10)
-            {
-                motor_update.MOTOR_turn_angle = -30;
-            }
-            else if(sensor_data.SENSOR_middle_sensor > 6 && sensor_data.SENSOR_middle_sensor < 15)
-            {
-                motor_update.MOTOR_speed = 0;
-                motor_update.MOTOR_turn_angle = 0;
-            }
-            dbc_encode_and_send_MOTOR_UPDATE(&motor_update);
-        }
-    }
+    LE.toggle(2);
 }
 
 void period_100Hz(uint32_t count)
 {
-    //LE.toggle(3);
+    LE.toggle(3);
 }
 
 // 1Khz (1ms) is only run if Periodic Dispatcher was configured to run it at main():
 // scheduler_add_task(new periodicSchedulerTask(run_1Khz = true));
 void period_1000Hz(uint32_t count)
 {
-    //LE.toggle(4);
+    LE.toggle(4);
 }
